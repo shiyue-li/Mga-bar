776ac5bb66fab8d4834b8a56c63677ba39ace534
shiyue-li <shiyue_li@brown.edu>
2020-07-17 18:44:55 -0400

    Fixed weight assigning function

--- stable_trees.py
+++ stable_trees.py
@@ -25,6 +25,20 @@ def count_leaves(trees):
     L =  [T for T in trees]
     return counts
 
+def new_label(vert_count, leaf_indexes, w, perm):
+    '''
+    input: the number of vertices, an iterator containing leaf indexes, a weight vector, a permutation.
+    output: an iterator of new labels using weight vector permuted by perm.
+    '''
+    new_w = perm.action(w)
+    new_l = []
+    for i in range(vert_count):
+        if i in leaf_indexes:
+            new_l.append(new_w[leaf_indexes.index(i)])
+        else:
+            new_l.append(None)
+    return new_l
+
 def assign_weights(T, w):
     '''
     input: a tree, and a weight vector (list).
@@ -35,21 +49,22 @@ def assign_weights(T, w):
     if T.degree().count(1) != len(w):
         raise Warning('Weight vector length must match leaf count.')
     else:
+        weighted_trees = []
+        print('assigning...')
         S = Permutations(len(w))
         leaf_indexes = [i for i, x in enumerate(T.degree()) if x == 1]
-
+        print('leaf index', leaf_indexes)
+        for perm in S:
+            label = new_label(len(T.get_vertices()),leaf_indexes, w, perm)
+            print('new label', label)
+            new_T = T
+            for i in range(len(label)):
+                print('index', i, 'label', label[i])
+                new_T.set_vertex(i, label[i])
+            print('new tree', new_T.get_vertices())
+            weighted_trees.append(new_T)
         # Assign weight to leaves while permuting the weight vector entries.
-        return [T.relabel(new_label(len(w), leaf_indexes, w, perm), inplace=False) for perm in S]
-
-
-def new_label(leaf_counts, leaf_indexes, w, perm):
-    '''
-    input: a leaf count, an iterator containing leaf indexes, a weight vector, a permutation.
-    output: an iterator of new labels using weight vector permuted by perm.
-    '''
-    new_w = perm.action(w)
-    print(new_w)
-    return [new_w[leaf_indexes.index(i)] if i in leaf_indexes else None for i in range(leaf_counts)]
+        return weighted_trees
 
 def assign_weights_to_forest(trees, w):
     '''
@@ -61,22 +76,14 @@ def assign_weights_to_forest(trees, w):
         weighted_trees.append(assign_weights(t, w))
     return weighted_trees
 
-
-### CREATE OF STABLE TREES OF A GIVEN WEIGHT###
-# Note:
-# V = E + 1
-# V = I (internal vertices) + L (leaves)
-# E = ((sum over I of deg v) + L) / 2 >= (3I + L ) / 2 by at least tri-valence of internal vertex
-# Thus, V <= 2L - 2
-
-# Useful test inputs
-w2 = [1, 1/2]
-w4= [1, 1, 1/1000, 1/1000]
-pg = graphs.PetersenGraph()
-F2 = TreeIterator(2)
-F3 = TreeIterator(3)
-F4 = TreeIterator(4)
-
+def draw_forest(forest):
+    '''
+    input: a forest.
+    output: plots of the forest.
+    '''
+    for T in forest:
+        g = T.plot()
+        g.show()
 
 def chop_trees(trees, n):
     '''
@@ -91,7 +98,7 @@ def chop_trees(trees, n):
             n_leaf_forest.append(T)
     return n_leaf_forest
 
-def n_leaf_trees(L):
+def n_leaf_forest(L):
     '''
     input: an integer L.
     output: an Iterator of trees with precisely n leaves.
@@ -101,8 +108,8 @@ def n_leaf_trees(L):
         trees = TreeIterator(V)
         for T in trees:
             forest.append(T)
-    n_leaf_trees = chop_trees(forest, L)
-    return n_leaf_trees
+    n_leaf_forest = chop_trees(forest, L)
+    return n_leaf_forest
 
 def neighbor_labels(v, weighted_tree):
     '''
@@ -115,6 +122,23 @@ def neighbor_labels(v, weighted_tree):
     vert_dic = weighted_tree.get_vertices()
     nbhrs = weighted_tree.neighbors(v)
     return [vert_dic[u] if vert_dic[u] != None else 1 for u in nbhrs]
+
+### CREATE OF STABLE TREES OF A GIVEN WEIGHT###
+# Note:
+# V = E + 1
+# V = I (internal vertices) + L (leaves)
+# E = ((sum over I of deg v) + L) / 2 >= (3I + L ) / 2 by at least tri-valence of internal vertex
+# Thus, V <= 2L - 2
+
+# Useful test inputs
+LM4 = [1, 1, 1/1000, 1/1000] # losev manin wv of size 4
+LM5 = [1, 1, 1/1000, 1/1000, 1/1000] # losev manin wv of size 5
+
+pg = graphs.PetersenGraph()
+F2 = TreeIterator(2)
+F3 = TreeIterator(3)
+F4 = TreeIterator(4)
+FL5 = n_leaf_forest(5)
 
 def is_stable(T):
         '''
